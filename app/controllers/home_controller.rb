4c3b667977266e6199c9beafe2d8dcc77e71fcd5
lm-afrin <titlee42@gmail.com>
2021-10-23 17:18:57 +0900

    adjust image based on backgroud image

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,12 +3,52 @@ include Magick
 class HomeController < ApplicationController
 
   def home
+    @images = []
+    imageList = ImageList.new
     params[:images].each_with_index do |image, index|
-      filename = "image-#{index}"
-      image_data = Base64.decode64(image[:url]['data:image/png;base64,'.length .. -1])
-      blob = ActiveStorage::Blob.create_and_upload!(io: StringIO.new(image_data), filename: filename)
-      blob.download
+      filename = "image-#{index}.jpeg"
+      # think about jpeg/png
+      image_data = Base64.decode64(image[:url]['data:image/jpeg;base64,'.length .. -1])
+      File.open("#{Rails.root}/tmp/storage/#{filename}", 'wb') do |f|
+        f.write image_data
+      end
+      file = File.open("#{Rails.root}/tmp/storage/#{filename}")
+      blob = ActiveStorage::Blob.create_and_upload!(io: file, filename: filename)
+      @images.push(image: ImageList.new("#{Rails.root}/tmp/storage/#{filename}"), width: image[:width], height: image[:height])
+      File.delete("#{Rails.root}/tmp/storage/#{filename}")
     end
+
+    bg = "#{Rails.root}/app/assets/images/bg.png"
+    read_bg = ImageList.new(bg)
+    bg_size = read_bg.columns
+    size = calculate_total_width_height(@images,"vertical")
+    if size > bg_size
+      diff = ((bg_size-size).abs)/@images.count
+      @images = adjust_image_sizes(@images, diff)
+    end
+    # padding = params[]
+    if params[:alignment] == "vertical"
+      size = 0
+      x = 0
+      @images = adjust_image_sizes(@images, 0)
+      @images.each do |image|
+        # logger.debug("@@@----@@@#{image[:width]}---#{image[:height]}")
+        image[:image].resize_to_fit!(image[:width], image[:height])
+        read_bg.composite!(image[:image], x+10, 0+10, OverCompositeOp)
+        x += image[:width]+10
+        size += image[:width]
+        # logger.debug("@--#{image[:width]}---#{image[:height]}")
+      end
+    # else
+    #   bg_size = read_bg.columns
+    #   images.each do |image|
+
+    #   end
+    end
+
+    read_bg.write("#{Rails.root}/app/assets/images/collage.png")
+
+
     # bg = "#{Rails.root}/app/assets/images/bg.png"
     # f1 = "#{Rails.root}/app/assets/images/flower1.png"
     # f2 = "#{Rails.root}/app/assets/images/flower2.png"
@@ -50,4 +90,34 @@ class HomeController < ApplicationController
     # read_bg.write("#{Rails.root}/app/assets/images/collage.png")
 
   end
+
+  def calculate_total_width_height(images,alignment)
+    sum = 0
+    if alignment == "vertical"
+      images.each do |image|
+        sum += image[:width]
+      end
+    end
+    return sum
+  end
+
+  def adjust_image_sizes(images, diff)
+    diff1 = (1600/images.count).floor
+    diff2 = 900
+    images.each do |image|
+      # ratio = [(image[:width]-diff)/image[:width], (image[:height]-diff)/image[:height]].min
+      # image[:width] = ratio*image[:width]
+      # image[:height] = ratio*image[:height]
+      w = diff1.to_f/image[:image].columns
+      h = diff2.to_f/image[:image].rows
+      ratio = [w, h].min
+      logger.debug("---ratio=#{ratio}---#{diff1/image[:image].columns}---#{image[:image].rows}")
+      image[:width] = ratio*image[:image].columns
+      image[:height] = ratio*image[:image].rows
+
+      logger.debug("-----#{image[:image].columns}----#{image[:image].rows}")
+    end
+    return images
+  end
+
 end
